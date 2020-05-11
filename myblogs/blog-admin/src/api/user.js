61e3da7e5edd6a174e8bdf8473ad906788c172ce
FORyc <952717308@qq.com>
2020-05-11 22:57:19 +0800

    接口地址修改

--- myblogs/blog-admin/src/api/user.js
+++ myblogs/blog-admin/src/api/user.js
@@ -2,7 +2,8 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    url: '/vue-admin-template/user/login',
+    // url: '/vue-admin-template/user/login',
+    url: '/admin/sys-user/login',
     method: 'post',
     data
   })
@@ -10,7 +11,8 @@ export function login(data) {
 
 export function getInfo(token) {
   return request({
-    url: '/vue-admin-template/user/info',
+    // url: '/vue-admin-template/user/info',
+    url: '/admin/sys-user/getInfo',
     method: 'get',
     params: { token }
   })
@@ -18,7 +20,8 @@ export function getInfo(token) {
 
 export function logout() {
   return request({
-    url: '/vue-admin-template/user/logout',
+    // url: '/vue-admin-template/user/logout',
+    url: '/admin/sys-user/logout',
     method: 'post'
   })
 }
